7c6c20ca5d15408c49ec19d0061657505c9b24a8
aquinary <github@aquinary.ru>
2022-12-02 01:50:15 +0300

    Создание каркаса и базовая вёрстка

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/layouts/footer/Footer";
+import Header from "./components/layouts/header/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      asdsad
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layouts/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="createdBy">Создано <a href="http://t.me/aquinary">@aquinary</a>, как проект для портфолио</div>
+      <div className="socialLink">ReactJS</div>
+    </div>
+  );
+}
